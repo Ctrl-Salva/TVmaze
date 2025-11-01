a3f241d243c042494d216c100d46c1baf6a2196f
Luis Fernando <louisborgerth@gmail.com>
2025-11-01 03:38:15 -0300

    0.8.1

--- src/main/java/com/example/tvmaze/dtos/pessoa/PessoaCriacaoDTO.java
+++ src/main/java/com/example/tvmaze/dtos/pessoa/PessoaCriacaoDTO.java
@@ -1,12 +1,6 @@
 package com.example.tvmaze.dtos.pessoa;
 
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
-
-import com.example.tvmaze.entities.Participacao;
-
-import jakarta.persistence.OneToMany;
 
 public class PessoaCriacaoDTO {
 
